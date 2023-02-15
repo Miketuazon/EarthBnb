17fe1ab8c51aa2356b1a8ba3d96dba8a5258416e
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-15 18:39:21 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -46,10 +46,47 @@ if (!isProduction) {
     })
   );
 
+// Phase 2 | Resource Not Found Error-Handler | Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+// Phase 2 | Sequelize Error-Handler | catching Sequelize errors, format them b4 sending err res
+const { ValidationError } = require('sequelize');
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+// Phase 2 | Error Formatter Error-Handler | formatting all the errors b4 returning a JSON res
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
+
+// Phase 2 | Testing the Error Handlers | Can test Resource Not Found error handler
+
 const routes = require('./routes');
-
-// ...
-
 app.use(routes); // Connect all the routes
 
 module.exports = app;
