86eb58ba2bee35b6580c5682d24868336dcb6661
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-20 15:44:15 -0500

    signup complete and tested

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,24 @@
 const express = require('express') //This file will hold the resources for the route paths beginning with /api/users.
 const router = express.Router();
+// helper functions
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
+// /api/users
+// Sign up
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
 
+      await setTokenCookie(res, user);
 
-
-
+      return res.json({
+        user: user
+      });
+    }
+ );
 
 
 module.exports = router;
