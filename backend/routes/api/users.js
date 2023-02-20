ef4ac11fce8fa7e0139931c35eb6aef1d87cfab3
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-20 17:17:27 -0500

    validate signup complete and tested

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,11 +3,36 @@ const router = express.Router();
 // helper functions
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+// Phase 5 | validating signup requests
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
 
 // /api/users
 // Sign up
 router.post(
     '/',
+    // P5 | added validateSignup to connect
+    validateSignup,
     async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ email, username, password });
