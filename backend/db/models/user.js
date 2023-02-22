4fa364d3b11e02dfa692bd077952daf4ffea01cc
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-22 10:39:45 -0500

    finished associations in models

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -49,6 +49,19 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
+      User.hasMany(models.Booking, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+        hooks: true,
+      });
+      User.hasMany(models.Spot, {
+        foreignKey: 'ownerId',
+      });
+      User.hasMany(models.Review, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+        hooks: true,
+      })
     }
   };
 
