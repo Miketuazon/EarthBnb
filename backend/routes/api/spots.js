fd227d170a10595007bf526d816f265967ed6f12
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-24 17:17:08 -0500

    added spotimages.id to grouping

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -82,7 +82,7 @@ router.get('/:spotId', async (req,res) => {
                 [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgStarRating'],
             ]
         },
-        group: ['Spot.id']
+        group: ['Spot.id', 'SpotImages.id']
     })
     res.json(spot);
 })
