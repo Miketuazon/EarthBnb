31eefd136fd93db3b46d914bbc2a22d7a6e2b11e
Miketuazon <michael.d.tuazon@gmail.com>
2023-02-27 09:08:56 -0500

    finished delete spot-i

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,8 @@ const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 const spotsRouter = require('./spots');
 const reviewsRouter = require('./reviews');
-const bookingsRouter = require('./bookings')
+const bookingsRouter = require('./bookings');
+const spotImagesRouter = require('./spot-images')
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
@@ -20,6 +21,7 @@ router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
 
+router.use('/spot-images', spotImagesRouter);
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 // });
---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,47 @@
+const express = require('express'); //This file will hold the resources for the route paths beginning with /api/spots.
+const router = express.Router();
+const { Spot, SpotImage, Review, ReviewImage, Booking, sequelize, User, Sequelize } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrorsForSpots } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
+
+// delete a spot image | URL: /api/spot-images/:imageId
+// ReqAuthen AND ReqPropAuthor
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const { user } = req;
+
+    let spotImage = await SpotImage.findByPk(req.params.imageId, {
+        include: [
+            {model: Spot}
+        ]
+    });
+
+    // Couldn't find a Spot Image with the specified id
+    if (!spotImage) {
+        return res.status(404).json({
+            message: "Spot couldn't be found",
+            statusCode: 404
+        })
+    };
+
+    let willDestroyImage = spotImage.toJSON();
+
+    // Spot must belong to the current user | Authorization
+    if (willDestroyImage.Spot.ownerId !== user.id) {
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: "403"
+        });
+    }
+
+    await spotImage.destroy();
+
+    res.status(200);
+    res.json({
+        message: "Successfully deleted",
+        statusCode: res.statusCode
+    })
+});
+
+
+module.exports = router;
