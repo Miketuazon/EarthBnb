04fac29dee9af320313fa66f33d46acafd262f1c
Miketuazon <michael.d.tuazon@gmail.com>
2023-03-10 18:08:44 -0500

    loginform component made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Phase1: import LoginFormPage component and then insert into App
+import LoginFormPage from './components/LoginFormPage';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
