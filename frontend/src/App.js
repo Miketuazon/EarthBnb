b4ef9dec3e5934a5734cd34419b9d114128244d0
Miketuazon <michael.d.tuazon@gmail.com>
2023-03-13 11:20:22 -0400

    login form modal complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from 'react-router-dom';
-// Phase1: import LoginFormPage component and then insert into App
-import LoginFormPage from './components/LoginFormPage';
-// Phase2: import SignupFormPage component and then insert into App
+import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
-// Phase3: import navigation component and then insert into app
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -21,9 +17,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
