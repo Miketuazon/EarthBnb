1d26adb86f04fab8f1346678d4962ff841c9bc8e
Miketuazon <michael.d.tuazon@gmail.com>
2023-06-21 14:42:44 -0400

    deleted unique spotId in BE for bookings. created modal for bookings. created disabled reserve button for owner if session user.

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -3,7 +3,7 @@ import { csrfFetch } from "./csrf";
 
 // Declare POJO action creator
 const LOAD_BOOKINGS_ONE_SPOT = "bookings/LoadBookingsOneSpot"
-
+const CREATE_BOOKING = "bookings/CreateBooking"
 
 // Store - action creators | Spots
 
@@ -14,18 +14,47 @@ export const loadBookings = (bookings) => {
     }
 }
 
+export const createBooking = (booking) => {
+    return {
+        type:  CREATE_BOOKING,
+        booking
+    }
+}
+
 // Store - Thunk | Spots
 // Thunk1: Get all bookings of spot
 
 export const loadBookingsThunk = (spotId) => async (dispatch) => {
     const res = await csrfFetch(`/api/spots/${spotId}/bookings`)
-
+    // console.log("res => ", res)
     if (res.ok) {
         const data = await res.json()
 
         await dispatch(loadBookings(data))
         return data
     }
+}
+
+// Thunk2: Create booking for a spot
+export const createBookingThunk = (bookingDetails, spotId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}/bookings`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(bookingDetails)
+    })
+
+    // console.log("res => ", res)
+    // console.log("res.json => ", await res.json())
+
+    if (res.ok) {
+        const createdBooking = await res.json()
+    } else {
+        const data = await res.json()
+        if (data.errors) {
+            return data.errors
+        }
+    }
+
 }
 
 const initialState = {
@@ -40,11 +69,16 @@ const bookingsReducer = (state = initialState, action) => {
                 ...state,
                 spot: {...state.spot}
             }
-            debugger
-            action.bookings.Bookings.map(booking => {
+            // debugger
+            action.bookings.Booking.map(booking => {
                 newState.spot[booking.id] = booking
             })
             return newState;
+        }
+        case CREATE_BOOKING: {
+            const newState = {...state,}
+            newState.spot[action.booking.id] = action.booking
+            return newState
         }
         default:
             return state;
